6e4bcaca7879e8030b826bd8a3921a911e7d37a1
Marco Preciado <mac46005@gmail.com>
2023-10-11 14:01:48 -0500

    Continue working on convertion for Temperature types

--- app/src/main/java/com/preciado/measurementconverter/data/interfaces/IMeasurementType.kt
+++ app/src/main/java/com/preciado/measurementconverter/data/interfaces/IMeasurementType.kt
@@ -2,4 +2,6 @@ package com.preciado.measurementconverter.data.interfaces
 
 interface IMeasurementType{
     var value: Double
+
+    fun convertTo(measurementType: IMeasurementType): IMeasurementType
 }
--- app/src/main/java/com/preciado/measurementconverter/data/models/Temperatures.kt
+++ app/src/main/java/com/preciado/measurementconverter/data/models/Temperatures.kt
@@ -1,25 +1,39 @@
 package com.preciado.measurementconverter.data.models
 
-import com.preciado.measurementconverter.data.interfaces.IConvertUnit
 import com.preciado.measurementconverter.data.interfaces.IMeasurementConverter
 import com.preciado.measurementconverter.data.interfaces.IMeasurementType
 
-sealed class Temperatures: IMeasurementConverter {
+class Temperatures: IMeasurementConverter {
+    override var Unit1: IMeasurementType = TemperatureUnit.Fahrenheit()
 
-    //fahrenheit to celsius
-    //(°F − 32) × 5/9 = 0°C
 
-    //celsius to fahrenheit
-    //(0°C × 9/5) + 32 = 32°F
+    override var Unit2: IMeasurementType = TemperatureUnit.Celsius()
+
+
 
 
     override fun convert(): Double {
-        TODO("Not yet implemented")
+        when(Unit1){
+            is TemperatureUnit.Fahrenheit -> {
+                Unit2 = (Unit1 as TemperatureUnit.Fahrenheit).convertTo(Unit2)
+            }
+            is TemperatureUnit.Celsius -> {
+                Unit2 = (Unit1 as TemperatureUnit.Celsius).convertTo(Unit2)
+            }
+            is TemperatureUnit.Kelvin -> {
+                Unit2 = (Unit1 as TemperatureUnit.Kelvin).convertTo(Unit2)
+            }
+        }
+        return
     }
 
 
 
+//fahrenheit to celsius
+    //(°F − 32) × 5/9 = 0°C
 
+    //celsius to fahrenheit
+    //(0°C × 9/5) + 32 = 32°F
 
 
 
@@ -28,14 +42,51 @@ sealed class Temperatures: IMeasurementConverter {
 
         class Fahrenheit: IMeasurementType{
             override var value: Double = 0.0
+            override fun convertTo(measurementType: IMeasurementType): IMeasurementType {
+                TODO("Not yet implemented")
+            }
+
+            private fun convertFahrenheitToCelsius(): Celsius {
+                return Celsius()
+            }
+
+            private fun convertFahrenheitToKelvin(): Kelvin{
+                return Kelvin()
+            }
         }
+
+
+
 
         class Celsius: IMeasurementType{
             override var value: Double = 0.0
+            override fun convertTo(measurementType: IMeasurementType): IMeasurementType {
+                TODO("Not yet implemented")
+            }
+
+            private fun convertCelsiusToFahrenheit(): Fahrenheit{
+                return Fahrenheit()
+            }
+
+            private fun convertCelsiusToKelvin(): Kelvin{
+                return Kelvin()
+            }
+
         }
 
         class Kelvin: IMeasurementType{
             override var value: Double = 0.0
+            override fun convertTo(measurementType: IMeasurementType): IMeasurementType {
+                TODO("Not yet implemented")
+            }
+
+            private fun convertKelvinToFahrenheit(): Fahrenheit{
+                return Fahrenheit()
+            }
+
+            private fun convertKelvinToCelsius(): Celsius{
+                return Celsius()
+            }
         }
     }
 }
