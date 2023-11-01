dacc1eee6a98cb428623eebe617a630eae61caa9
Marco Preciado <mac46005@gmail.com>
2023-11-01 11:04:25 -0500

    update length units

--- app/src/main/java/com/preciado/measurementconverter/data/models/lengths/Inch.kt
+++ app/src/main/java/com/preciado/measurementconverter/data/models/lengths/Inch.kt
@@ -27,35 +27,48 @@ class Inch: LengthUnit("Inch"), IToLengthUnit {
 
     override fun toDecameter(): Decameter {
         val decameter = Decameter()
-
+        decameter.value = value / 393.7
+        return decameter
     }
 
     override fun toHectometer(): Hectometer {
-        TODO("Not yet implemented")
+        val hectometer = Hectometer()
+        hectometer.value = value / 3937
+        return hectometer
     }
 
     override fun toKilometer(): Kilometer {
-        TODO("Not yet implemented")
+        val kilometer = Kilometer()
+        kilometer.value = value / 39370
+        return kilometer
     }
 
     override fun toInch(): Inch {
-        TODO("Not yet implemented")
+        return this
     }
 
     override fun toFoot(): Foot {
-        TODO("Not yet implemented")
+        val foot = Foot()
+        foot.value = value / 12
+        return foot
     }
 
     override fun toMile(): Mile {
-        TODO("Not yet implemented")
+        val mile = Mile()
+        mile.value = value / 63360
+        return mile
     }
 
     override fun toYard(): Yard {
-        TODO("Not yet implemented")
+        val yard = Yard()
+        yard.value = value / 36
+        return yard
     }
 
     override fun toNauticalMile(): NauticalMile {
-        TODO("Not yet implemented")
+        val nauticalMile = NauticalMile()
+        nauticalMile.value = value / 72910
+        return nauticalMile
     }
 
 }
--- app/src/main/java/com/preciado/measurementconverter/data/models/lengths/Kilometer.kt
+++ app/src/main/java/com/preciado/measurementconverter/data/models/lengths/Kilometer.kt
@@ -1,4 +1,74 @@
 package com.preciado.measurementconverter.data.models.lengths
 
-class Kilometer: LengthUnit(name = "Kilometer") {
+class Kilometer: LengthUnit(name = "Kilometer"), IToLengthUnit {
+    override fun toMillimeter(): Millimeter {
+        val millimeter = Millimeter()
+        millimeter.value = value * 1000000
+        return millimeter
+    }
+
+    override fun toCentimeter(): Centimeter {
+        val centimeter = Centimeter()
+        centimeter.value = value * 100000
+        return centimeter
+    }
+
+    override fun toDecimeter(): Decimeter {
+        val decimeter = Decimeter()
+        decimeter.value = value * 10000
+        return decimeter
+    }
+
+    override fun toMeter(): Meter {
+        val meter = Meter()
+        meter.value = value * 1000
+        return meter
+    }
+
+    override fun toDecameter(): Decameter {
+        val decameter = Decameter()
+        decameter.value = value * 100
+        return decameter
+    }
+
+    override fun toHectometer(): Hectometer {
+        val hectometer = Hectometer()
+        hectometer.value = value * 10
+        return hectometer
+    }
+
+    override fun toKilometer(): Kilometer {
+        return this
+    }
+
+    override fun toInch(): Inch {
+        val inch = Inch()
+        inch.value = value * 39370
+        return inch
+    }
+
+    override fun toFoot(): Foot {
+        val foot = Foot()
+        foot.value = value * 3281
+        return foot
+    }
+
+    override fun toMile(): Mile {
+        val mile = Mile()
+        mile.value = value / 1.609
+        return mile
+    }
+
+    override fun toYard(): Yard {
+        val yard = Yard()
+        yard.value = value * 1094
+        return yard
+    }
+
+    override fun toNauticalMile(): NauticalMile {
+        val nauticalMile = NauticalMile()
+        nauticalMile.value = value / 1.852
+        return nauticalMile
+    }
+
 }
